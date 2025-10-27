ebb7eca718525c60e0cb532b96eba080607c81fd
Gabriel Eduardo de Souza <gabrielsouzapequi@gmail.com>
2025-10-27 18:09:38 -0300

    feat: add User entity

---
+++ apps/auth-service/src/users/models/user.entity.ts
@@ -0,0 +1,27 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @Column({ nullable: true })
+  currentHashedRefreshToken?: string;
+}
